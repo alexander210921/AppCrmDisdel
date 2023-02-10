f88ab3e3703cc5c807ce5452884abebb675f1045
Franklin Simalij <alexandersimalij12@gmail.com>
2023-02-10 11:44:31 -0600

    drawing line in the map

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -1,14 +1,20 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import MapView, {Marker} from 'react-native-maps';
 import {useDispatch, useSelector} from 'react-redux';
 import {StyleSheet, View} from 'react-native';
-import { SetDestinationCoords } from '../../Api/User/ApiUser';
+import {SetDestinationCoords} from '../../Api/User/ApiUser';
+import MapViewDirections from 'react-native-maps-directions';
 export const RenderMap = () => {
   const user = useSelector(state => state.login);
+  const [coordsDestination, setCoordsDestination] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
   const dispatch = useDispatch();
-  const SaveVisitDestination=(e)=>{
-    dispatch(SetDestinationCoords(e.nativeEvent.coordinate))
-  }
+  const SaveVisitDestination = e => {
+    dispatch(SetDestinationCoords(e.nativeEvent.coordinate));
+    setCoordsDestination(e.nativeEvent.coordinate);
+  };
   return (
     <View style={styles.container}>
       <MapView
@@ -19,6 +25,20 @@ export const RenderMap = () => {
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
+        {coordsDestination.latitude > 0 ? (
+          <MapViewDirections
+            origin={{
+              latitude: user.coordsActuality.latitude,
+              longitude: user.coordsActuality.longitude,
+            }}
+            destination={coordsDestination}
+            apikey={
+              'AIzaSyBGzUb5aIQyMpPPaBNZz9CJXvuQDajqavs'
+            }
+            strokeWidth={3}
+            strokeColor="red"
+            ></MapViewDirections>
+        ) : null}
         <Marker
           draggable
           coordinate={{
