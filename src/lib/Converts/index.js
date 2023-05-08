4fc8ab4708002b71a45703cdc538718ff67c4a5d
Franklin Simalij <alexandersimalij12@gmail.com>
2023-05-08 07:45:24 -0600

    delete blop

--- src/lib/Converts/index.js
+++ src/lib/Converts/index.js
@@ -1,12 +1,3 @@
-import RNFetchBlob from 'rn-fetch-blob';
 export function _base64ToArrayBuffer(base64) {    
    return  null;
-}
-export async function sendFileToServer(fileUri) {  
- try{
-   const CHUNK_SIZE = 1024 * 1024; // 1 MB
-   const readStream = await RNFetchBlob.fs.readStream(fileUri, 'base64', CHUNK_SIZE);      
- }catch(ex){
-   console.log(ex,"Error al abrir la imagen")
- }
 }
