af9388aecdf6d5a759e701d88647b56b6b6d74fe
Franklin Simalij <alexandersimalij12@gmail.com>
2023-05-10 10:48:55 -0600

    add ListComponent

--- src/Views/Product/ListProduct/ListProduct.js
+++ src/Views/Product/ListProduct/ListProduct.js
@@ -1,6 +1,5 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useMemo} from 'react';
 import {
-  View,
   Text,
   FlatList,
   ActivityIndicator,
@@ -10,8 +9,7 @@ import {
 } from 'react-native';
 import {StyleSheet, Image} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {PageControl, Colors} from 'react-native-ui-lib';
-// import { LazyloadScrollView, LazyloadView } from 'react-native-lazyload';
+import {PageControl, Colors, View} from 'react-native-ui-lib';
 const ListProduct = ({ListData}) => {
   const [loading, setLoading] = useState(false);
   const [searchText, setSearchText] = useState('');
@@ -22,22 +20,28 @@ const ListProduct = ({ListData}) => {
   const indexOfLastTodo = currentPage * todosPerPage;
   const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
   const [quantity, setQuantity] = useState(1);
-  const [QuantityItems,setQuantityItems]=useState(0);
-  const currentTodos =
-    filteredItems.length > 0
+  const [QuantityItems, setQuantityItems] = useState(0);
+  const currentTodos = useMemo(() => {
+    return filteredItems.length > 0
       ? filteredItems.slice(indexOfFirstTodo, indexOfLastTodo)
-      : [];  
+      : [];
+  }, [currentPage, filteredItems]);
+
   const handleSearch = text => {
     setSearchText(text);
   };
   const onSearch = () => {
-    if(searchText==null || searchText ===""){return;}    
-    const filtered = ListData.filter(item =>
-      item.Descripcion.toLowerCase().includes(searchText.toLowerCase()),
+    if (searchText == null || searchText === '') {
+      return;
+    }
+    const filtered = ListData.filter(
+      item =>
+        item.Descripcion.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.IdProducto.toLowerCase().includes(searchText.toLowerCase()),
     );
     setFilteredItems(filtered);
     setQuantityItems(filtered.length);
-    //setCurrentPage(1);
+    setCurrentPage(1);
   };
   const handleQuantityChange = newQuantity => {
     setQuantity(newQuantity);
@@ -74,8 +78,9 @@ const ListProduct = ({ListData}) => {
           style={styles.image}
           resizeMode="cover"
         />
+        <Text style={{color: 'gray'}}>{item.IdProducto}</Text>
         <Text style={styles.description}>{item.Descripcion}</Text>
-        <Text style={styles.price}>Q. {item.price}</Text>
+        <Text style={styles.price}>Q. {item.PrecioDeLista}</Text>
         <View style={styles.quantityContainer}>
           <TouchableOpacity
             onPress={() => handleQuantityChange(quantity - 1)}
@@ -107,7 +112,7 @@ const ListProduct = ({ListData}) => {
           <TextInput
             style={styles.input}
             placeholder={'Buscar Producto'}
-            //value={searchText}            
+            //value={searchText}
             onChangeText={handleSearch}
             onBlur={onSearch}
             placeholderTextColor={'#b3b2b7'}
@@ -115,7 +120,7 @@ const ListProduct = ({ListData}) => {
         </View>
         <FlatList
           data={currentTodos}
-          keyExtractor={(item, index) => index.toString()}
+          keyExtractor={item => item.IdProducto.toString()}
           renderItem={({item}) => <Card item={item}></Card>}
           //onEndReached={fetchData}
           onEndReachedThreshold={0.7}
@@ -124,7 +129,6 @@ const ListProduct = ({ListData}) => {
         />
         <View>
           <PageControl
-            
             size={12}
             spacing={8}
             inactiveColor={Colors.grey60}
@@ -135,7 +139,42 @@ const ListProduct = ({ListData}) => {
             containerStyle={{marginBottom: 40}}
             numOfPages={Math.ceil(QuantityItems / todosPerPage)}
             currentPage={currentPage - 1}
+            enlargeActive={true}
           />
+          {currentTodos && currentTodos.length > 0 ? (
+            <View style={{margin: 5}} row flex centerH>
+              {/* <Button style={{backgroundColor:'gray'}} label={'anterior'} onPress={() => {}} disabled={false} /> */}
+              <TouchableOpacity
+                onPress={() => {
+                  setCurrentPage(currentPage - 1);
+                }}
+                disabled={currentPage === 1}>
+                <Icon
+                  name={'arrow-left-bold-box'}
+                  size={30}
+                  color="#c06500"
+                  style={styles.icon}
+                />
+              </TouchableOpacity>
+
+              <Text style={{color: 'gray'}}>{currentPage}</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  setCurrentPage(currentPage + 1);
+                }}
+                disabled={
+                  currentPage === Math.ceil(QuantityItems / todosPerPage)
+                }>
+                <Icon
+                  name={'arrow-right-bold-box'}
+                  size={30}
+                  color="#c06500"
+                  style={styles.icon}
+                />
+              </TouchableOpacity>
+              {/* <Button  style={{backgroundColor:'gray',with:20}} label={'Siguiente'} onPress={() => {}} disabled={false} /> */}
+            </View>
+          ) : null}
         </View>
       </View>
     </ScrollView>
@@ -205,19 +244,19 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderColor: '#ccc',
     padding: 12,
-    marginBottom: 5,
+    marginBottom: 3,
     width: '45%',
     margin: '2%',
   },
   image: {
     width: '100%',
     height: 150,
-    marginBottom: 4,
+    marginBottom: 2,
   },
   description: {
     fontSize: 14,
     fontWeight: 'bold',
-    marginBottom: 4,
+    marginBottom: 1,
     color: 'black',
   },
   price: {
@@ -228,7 +267,7 @@ const styles = StyleSheet.create({
   quantityContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 12,
+    marginBottom: 5,
     alignSelf: 'center',
   },
   quantityButton: {
@@ -255,7 +294,7 @@ const styles = StyleSheet.create({
   },
   addButtonText: {
     color: '#fff',
-    fontSize: 14,
+    fontSize: 13,
     fontWeight: 'bold',
   },
 });
